1a86454dedd422e2b14209e7edc96d8ab45e0f93
denzl <lashindenislb232@gmail.com>
2025-12-15 21:56:34 +0300

    Created basic index view to fetch trips from database

--- station/views.py
+++ station/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Trip, City
 
-# Create your views here.
+def index(request):
+    # Рейсы из базы
+    trips = Trip.objects.all().order_by('departure_time')
+    # Города для формы поиска
+    cities = City.objects.all()
+
+    context = {
+        'trips': trips,
+        'cities': cities,
+    }
+    return render(request, 'station/index.html', context)
