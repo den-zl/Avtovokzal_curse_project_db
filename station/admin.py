65d4f5df986eccc3f6652f2bb81ed7ca8f6875c8
denzl <lashindenislb232@gmail.com>
2025-12-15 19:15:10 +0300

    Create superuser, admin panel configuration

--- station/admin.py
+++ station/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+# Импорт всех моделей из models.py
+from .models import (
+    City, BusModel, Bus, Driver, BusStation,
+    Route, Trip, UserProfile, Ticket, Payment, Review
+)
 
-# Register your models here.
+# Регистрация каждой модели, чтобы она появилась в интерфейсе
+admin.site.register(City)
+admin.site.register(BusModel)
+admin.site.register(Bus)
+admin.site.register(Driver)
+admin.site.register(BusStation)
+admin.site.register(Route)
+admin.site.register(Trip)
+admin.site.register(UserProfile)
+admin.site.register(Ticket)
+admin.site.register(Payment)
+admin.site.register(Review)
