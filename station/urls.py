af2192a60ea918a0d2fd95fbdd38e168c315a38d
denzl <lashindenislb232@gmail.com>
2025-12-16 12:22:34 +0300

    Added ticket booking logic and passenger personal area

--- station/urls.py
+++ station/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'), # Главная страница
     path('register/', views.register, name='register'),
     path('accounts/', include('django.contrib.auth.urls')), # login/logout
+    path('buy/<int:trip_id>/', views.buy_ticket, name='buy_ticket'),  # Ссылка на покупку
+    path('profile/', views.profile, name='profile'),  # Личный кабинет
 ]
--- station/views.py
+++ station/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from .models import Trip, City
+from .models import Trip, City, Ticket, Payment, UserProfile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from .models import Trip, Ticket, Payment
+import random
 
 def index(request):
     # Рейсы из базы
@@ -21,8 +24,28 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            UserProfile.objects.create(user=user, role='client')
             login(request, user)
             return redirect('index')
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
+@login_required
+def buy_ticket(request, trip_id):
+    trip = Trip.objects.get(id=trip_id)
+
+    ticket = Ticket.objects.create(
+        trip=trip,
+        passenger=request.user,
+        seat_number=random.randint(1, 50)
+    )
+
+    Payment.objects.create(ticket=ticket, amount=trip.price, is_paid=True)
+
+    return redirect('profile')
+
+@login_required
+def profile(request):
+
+    tickets = Ticket.objects.filter(passenger=request.user).order_by('-booking_date')
+    return render(request, 'station/profile.html', {'tickets': tickets})
