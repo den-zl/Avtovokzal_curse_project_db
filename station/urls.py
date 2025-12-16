6faca5bc360a965f0375dbc1bbd1afb283f2cca3
denzl <lashindenislb232@gmail.com>
2025-12-16 09:24:17 +0300

    Creating a passenger authentication system. Login and registration

--- station/urls.py
+++ station/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), # Главная страница
+    path('register/', views.register, name='register'),
+    path('accounts/', include('django.contrib.auth.urls')), # login/logout
 ]
--- station/views.py
+++ station/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Trip, City
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 def index(request):
     # Рейсы из базы
@@ -12,3 +15,14 @@ def index(request):
         'cities': cities,
     }
     return render(request, 'station/index.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
